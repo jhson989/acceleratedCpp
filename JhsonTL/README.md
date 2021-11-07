9b4d5f4a61b56e338a3bb0442d0e536ec5d40c13
Janghyun Son <jhson989@gmail.com>
2021-11-07 16:42:51 +0900

    Update test routine

---
+++ JhsonTL/README.md
@@ -0,0 +1,10 @@
+# JhsonTL
+## C++ Template Lirary by Janghyun Son
+
+1. mkdir build && cd build
+2. cmake .. && make
+3. ./app.out
+
+
+
+
--- JhsonTL/main.cpp
+++ JhsonTL/main.cpp
@@ -4,31 +4,46 @@
 #include "Vector.hpp"
 
 
+void test_vector ();
+
+
 int main(void) {
+
+    test_vector ();
+
+    return 0;
+}
+
+
+void test_vector () {
+
+    // Define a vector
     Vec<int> a;
 
+    // push_back
     for (int i=0; i<10; i++)
         a.push_back (i);
 
-
+    // Assignment initialization
     std::cout << "----B----" << std::endl;
     Vec<int> b = a;
     for (int i=0; i<10; i++)
         std::cout << b[i] << std::endl;
 
+    // Copy initialization
     std::cout << "----C----" << std::endl;
     Vec<int> c(b);
     for (int i=0; i<10; i++)
         std::cout << c[i] << std::endl;
 
+    // Assignment
     std::cout << "----B----" << std::endl;
     b = a;
     for (int i=0; i<10; i++)
         std::cout << b[i] << std::endl;
 
+    // Call Deconstructors
 
-
-
-    return 0;
 }
+
 
