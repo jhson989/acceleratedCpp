004c524ff0362b918f7e1bca009d51559ff467bc
Janghyun Son <jhson989@gmail.com>
2021-11-07 15:28:45 +0900

    Implement a custom vector

--- Vector/Vector.hpp
+++ Vector/Vector.hpp
@@ -1,7 +1,117 @@
-#pragma once
+
+#ifndef __VEC__
+#define __VEC__
+
+#include <iostream>
+#include <memory>
+#include <algorithm>
+
+template <class T>
+class Vec {
+
+public:
+    
+    // Class types
+    typedef T* iterator;
+    typedef const T* const_iterator;
+    typedef std::size_t size_type;
+    typedef T value_type;
+    typedef std::ptrdiff_t difference_type;
+    typedef T& reference;
+    typedef const T& const_reference;
+
+    /** Constructors **/
+    Vec () {create ();}
+    explicit Vec (size_type n, const_reference val = T()) {create (n, val);}
+    Vec (const Vec& v) {create (v.begin (), v.end ());}
+    
+    /** Destructor **/
+    ~Vec () {uncreate ();}
+
+    /** Vec operators **/
+    // Size function
+    size_type size () const {return (avail-data);}
+    
+    // Indexing operator
+    reference operator[] (size_type i) {return data[i];}
+    const_reference operator[] (size_type i) const {return data[i];}
+
+    // Iterator function
+    iterator begin() {return data;}
+    const_iterator begin() const {return data;}
+    iterator end() {return avail;}
+    const_iterator end() const {return avail;}
+
+    // Assignment operator
+    Vec& operator= (const Vec& rhs)  {
+        if (&rhs != this) {
+            uncreate ();
+            create (rhs.begin(), rhs.end());
+        }
+        return *this;
+    }
+
+    // Manipulation operator
+    void push_back (const_reference val) {
+        if (avail == limit) {
+            grow ();
+        }
+
+        unchecked_append (val);
+    }
 
 
-template <typename T>
-class Vector {
+private:
+    
+    iterator data;
+    iterator limit;
+    iterator avail;
+
+    std::allocator<T> alloc;
+
+    void create () {
+        data = limit = avail = 0;
+    }
+    
+    void create (size_type n, const_reference val) {
+        data = alloc.allocate (n);
+        limit = avail = data + n;
+        std::uninitialized_fill (data, limit, val);
+    }
+
+    void create (const_iterator begin, const_iterator end) {
+        data = alloc.allocate (end-begin);
+        limit = avail = std::uninitialized_copy (begin, end, data);
+    }
+
+    void uncreate () {
+        if (data) {
+            iterator it = avail;
+            while (it != data) {
+                alloc.destroy (--it);
+            }
+            alloc.deallocate (data, limit-data);
+        }
+        data = limit = avail = 0;
+    }
+
+    void grow () {
+        size_type new_size = std::max (2*(limit-data), difference_type (1));
+
+        iterator new_data = alloc.allocate(new_size);
+        iterator new_avail = std::uninitialized_copy (data, avail, new_data);
+
+        uncreate ();
+    
+        data = new_data;
+        avail = new_avail;
+        limit = data + new_size;
+    }
+
+    void unchecked_append (const_reference val) {
+        alloc.construct (avail++, val);
+    }
 
 };
+
+#endif
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,9 +1,32 @@
-
+#include "Vector.hpp"
 #include <iostream>
 
-#include "Vector.hpp"
 
-int main (int argc, char* argv[]) {
+int main(void) {
+    Vec<int> a;
+
+    for (int i=0; i<10; i++)
+        a.push_back (i);
+
+
+    std::cout << "----B----" << std::endl;
+    Vec<int> b = a;
+    for (int i=0; i<10; i++)
+        std::cout << b[i] << std::endl;
+
+    std::cout << "----C----" << std::endl;
+    Vec<int> c(b);
+    for (int i=0; i<10; i++)
+        std::cout << c[i] << std::endl;
+
+    std::cout << "----B----" << std::endl;
+    b = a;
+    for (int i=0; i<10; i++)
+        std::cout << b[i] << std::endl;
+
+
+
 
     return 0;
 }
+
